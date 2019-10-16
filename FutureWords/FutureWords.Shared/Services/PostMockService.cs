8dcd687432ffab57c4d4bbc2bb26b267735de14e
Elena Guzman <elena.guzbla@gmail.com>
2019-10-16 16:10:18 +0200

    Create IPostService and mocked data

---
+++ FutureWords/FutureWords.Shared/Services/PostMockService.cs
@@ -0,0 +1,23 @@
+﻿using FutureWords.Shared.Models;
+using System;
+using System.Collections.Generic;
+
+namespace FutureWords.Shared.Services
+{
+    public class PostMockService : IPostService
+    {
+        public Post GetPost()
+        {
+            return new Post { Title = "Title 2", Category = "XR", Content = "Content 2", PublicationDate = DateTime.Now };
+        }
+
+        public List<Post> GetPosts()
+        {
+            return new List<Post>
+            {
+                new Post { Title = "Title", Category = "XR", Content = "Content", PublicationDate = DateTime.Now },
+                new Post { Title = "Title 2", Category = "XR", Content = "Content 2", PublicationDate = DateTime.Now }
+            };
+        }
+    }
+}
