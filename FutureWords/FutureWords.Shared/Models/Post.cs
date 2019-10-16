2f857fd7bd214ad0334a0c3add7ba8592a85d6da
Elena Guzman <elena.guzbla@gmail.com>
2019-10-16 16:07:13 +0200

    Create Post model

---
+++ FutureWords/FutureWords.Shared/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FutureWords.Shared.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public string Link { get; set; }
+        public DateTime PublicationDate { get; set; }
+        
+    }
+}
